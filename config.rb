5ba0e321a8651064433db2f87871de7b20c07605
Chris Blow <cgblow@gmail.com>
2014-06-20 17:49:32 -0700

    move to www

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "www/stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "www/images"
+javascripts_dir = "www/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
