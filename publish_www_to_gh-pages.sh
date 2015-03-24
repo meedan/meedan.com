825ce27f3853d1c93ab0339d37aa89bb7d9eb506
Benjamin Foote <git@bnf.net>
2015-03-23 18:21:38 -0700

    publish www to gh-pages

---
+++ publish_www_to_gh-pages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+# checkout gh-pages branch
+git checkout gh-pages
+
+# and get rid of everything
+git rm -rf .
+
+# then cherry pick www content here
+git checkout master -- www
+
+# and move it into place
+git mv www/* ./
+git rm -rf www
+rm -rf www  # just for good measure
+
+# commit the change
+git commit -m "website update $(date -I)";
+
+# switch back to master
+git checkout master
+
+echo <<EOF
+
+   gh-pages branch has been updated with the content from ./www
+
+   you should take a look at that branch by running:
+         git checkout gh-pages
+
+   if all looks good you can publish these changes with
+         git push --all origin
+
+EOF
