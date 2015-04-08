89585e6d8549eb7e1a9e41f0ed2a7c67ca47276b
Chris Blow <unthinkingly@gmail.com>
2015-04-07 18:58:02 -0700

    drop IE 8 and 7, compress CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -138,13 +138,13 @@ gulp.task('dustreload', ['dust'], function () {
 var sassConfig = {
   errLogToConsole: true,
   includePaths: ["bower_components"],
-  outputStyle: "expanded"
+  outputStyle: "compressed"
 }
 
 gulp.task('sass', function () {
   return gulp.src(scssFiles)
     .pipe(sass(sassConfig))
-    .pipe(autoprefixer("last 4 versions", "> 1%", "ie 8", "ie 7"))
+    .pipe(autoprefixer("last 4 versions", "> 1%"))
     .pipe(gulp.dest(cssCompileDir))
     .pipe(reload({
       stream: true
