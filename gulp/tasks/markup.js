47380ff190718784ca33efbb936207bcc44f3cad
Chris Blow <cgblow@gmail.com>
2015-04-28 16:47:43 -0700

    add basic product pages

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,21 +1,15 @@
-var gulp = require('gulp');
-var dust = require('gulp-dust');
-dust.helpers = require('dustjs-helpers').helpers;
-var dusthtml = require('gulp-dust-html');
-var changed = require('gulp-changed');
+var gulp         = require('gulp');
+var dust         = require('gulp-dust');
+dust.helpers     = require('dustjs-helpers').helpers;
+var dusthtml     = require('gulp-dust-html');
+var changed      = require('gulp-changed');
 var markupConfig = require('../config').markup;
-var config = require('../config');
 var browserSync  = require('browser-sync');
-var dustFiles =
-  [
-    markupConfig.settings.basePath + '/*.dust',
-    markupConfig.settings.basePath + '/!src/_*.dust'
-  ];
 
 gulp.task('markup', function () {
   console.log(markupConfig.settings);
-  return gulp.src("src/markup/**/*.dust")
-    .pipe(changed(markupConfig.dest))
+  console.log(markupConfig.dustFiles);
+  return gulp.src(markupConfig.src)
     .pipe(dusthtml(markupConfig.settings))
     .pipe(gulp.dest(markupConfig.dest))
     .pipe(browserSync.reload({stream:true}));
