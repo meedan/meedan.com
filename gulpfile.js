0c4f1c8d86c3308ddba7e2d987a2eea4d3b6bc93
Chris Blow <cgblow@gmail.com>
2015-04-28 17:30:06 -0700

    update images

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,3 @@
 var gulp = require('gulp');
-// var rename = require('gulp-rename');
-// var concat = require('gulp-concat');
-// var pkg = require('package.json');
-
 var requireDir = require('require-dir');
-// Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
-
-// var parallel = require('concurrent-transform');
