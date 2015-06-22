d3a418bab060f7e840ac3ede890052e95a4baaad
Chris Blow <cgblow@gmail.com>
2015-06-22 14:53:25 -0700

    fix pagespeed task

--- gulp/tasks/pagespeed.js
+++ gulp/tasks/pagespeed.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var psi = require('psi');
+var site = "http://meedan.com";
 
 gulp.task('pagespeed', function () {
-    return psi(site, {
-        nokey: 'true',
-        strategy: 'mobile',
-    }, function (err, data) {
-        console.log(data.score);
-        console.log(data.pageStats);
-    });
+  return psi(site, {
+    nokey: 'true',
+    strategy: 'mobile',
+  }, function (err, data) {
+    console.log(data.score);
+    console.log(data.pageStats);
+  });
 });
