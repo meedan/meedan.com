4af490cfed4f508b18141b6451a1cedc4ae5d0d9
Chris Blow <cgblow@gmail.com>
2015-06-29 21:54:44 -0700

    updated bridge screenshot, updated google analytics script

--- gulp/tasks/documentation.js
+++ gulp/tasks/documentation.js
@@ -3,15 +3,14 @@ var gulp = require('gulp');
 var config = require('../config');
 var pageres = require('pageres');
 
-var pageres = new pageres({
-    delay: 2
-  })
-  .src('meedan.com', ['480x320', '1024x768', 'iphone 5s'], {
-    crop: true
-  })
-  .src('meedan.com/checkdesk-en', ['1280x1024', '1920x1080'])
-  .dest('docs/screenshots');
-
+// var pageres = new pageres({
+//     delay: 2
+//   })
+//   .src('meedan.com', ['480x320', '1024x768', 'iphone 5s'], {
+//     crop: true
+//   })
+//   .src('meedan.com/checkdesk-en', ['1280x1024', '1920x1080'])
+//   .dest('docs/screenshots');
 gulp.task('documentation', function () {
   pageres.run(function (err) {
     console.log('Done screenshotting ...');
