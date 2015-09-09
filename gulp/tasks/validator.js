5c79851e1c19b102b21a3fef34997e43262ff7d1
Chris Blow <cgblow@gmail.com>
2015-09-08 17:23:28 -0700

    Update bridge meta tags to use improved logo.

--- gulp/tasks/validator.js
+++ gulp/tasks/validator.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var w3cjs = require('gulp-w3cjs');
+// var gutil = require('gulp-util');
 
 // HTML Validator
 gulp.task('validate', function () {
   gulp.src(['www/*.html', '!www/_*.html'])
-    .on('error', handleErrors)
+    .on('error', function (err) {
+      console.log(err.message);
+    })
     .pipe(w3cjs());
 });
