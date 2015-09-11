799cfc9056efdbb6ab695af1484a04f3a475337e
xrwang <xiaowei.wang@post.harvard.edu>
2015-09-11 00:27:47 -0700

    Revert "Update bridge meta tags to use improved logo."
    This reverts commit 5c79851e1c19b102b21a3fef34997e43262ff7d1.

--- gulp/tasks/validator.js
+++ gulp/tasks/validator.js
@@ -1,12 +1,9 @@
 var gulp = require('gulp');
 var w3cjs = require('gulp-w3cjs');
-// var gutil = require('gulp-util');
 
 // HTML Validator
 gulp.task('validate', function () {
   gulp.src(['www/*.html', '!www/_*.html'])
-    .on('error', function (err) {
-      console.log(err.message);
-    })
+    .on('error', handleErrors)
     .pipe(w3cjs());
 });
