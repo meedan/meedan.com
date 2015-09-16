be3b8e50ecf2045d1c181db64dd45253bcfdd151
Chris Blow <cgblow@gmail.com>
2015-09-15 21:45:34 -0700

    start of localization WIP

--- config.rb
+++ config.rb
@@ -55,6 +55,8 @@ set :images_dir, 'images'
 
 activate :directory_indexes
 
+activate :i18n, :mount_at_root => "en"
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
