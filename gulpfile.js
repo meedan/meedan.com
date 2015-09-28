4e890a442b98b9acd86c77e32cbc259558f6e434
Chris Blow <cgblow@gmail.com>
2015-09-28 15:38:31 -0700

    remove most gulp tasks, add svg images

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var psi = require('psi');
+var site = "http://meedan.com";
+var w3cjs = require('gulp-w3cjs');
+
+gulp.task('bundle-svg', function () {
+  // SVG files get special handling
+  gulp.src('source/images/vector/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore())
+    .pipe(gulp.dest('sources/images/vector/'))
+});
+
+gulp.task('pagespeed', function () {
+  return psi(site, {
+    nokey: 'true',
+    strategy: 'mobile',
+  }, function (err, data) {
+    console.log(data.score);
+    console.log(data.pageStats);
+  });
+});
+
+// HTML Validator
+gulp.task('validate', function () {
+  gulp.src(['www/*.html', '!www/_*.html'])
+    .on('error', handleErrors)
+    .pipe(w3cjs());
+});
