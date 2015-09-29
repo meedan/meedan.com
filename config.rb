f158a0e5638010921adc47df678d5d4c166d8171
Chris Blow <cgblow@gmail.com>
2015-09-29 09:59:59 -0700

    language toggle WIP

--- config.rb
+++ config.rb
@@ -41,12 +41,30 @@ configure :development do
 	redirect "index.html", :to => "en/index.html"
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+
+	# Language toggle WIP
+	# https://forum.middlemanapp.com/t/i18n-list-of-language-siblings-and-links-to-them/978/2
+  def translated_url(locale)
+    # Assuming /:locale/page.html
+    page_name = @page_id.split("/", 2).last.sub(/\..*$/, '')
+
+    untranslated_path = t(:paths).index(page_name)
+
+    begin
+      translated = I18n.translate!("paths.#{untranslated_path}", locale: locale)
+    rescue I18n::MissingTranslationData
+      translated = untranslated_path
+    end
+
+    "page name is #{page_name} ... untranslated_path is #{untranslated_path} ... /#{locale}/#{translated}"
+  end
+
+  def other_langs
+    langs - [I18n.locale]
+  end
+
+end
 
 set :css_dir, 'css'
 
