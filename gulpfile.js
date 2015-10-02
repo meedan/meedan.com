0b1bdddb23b8a965ad59d29f58d50c997d45ddfc
Chris Blow <cgblow@gmail.com>
2015-10-01 17:26:59 -0700

    remove gulp validate since it is tested in html-proofer

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,26 @@ var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
 var psi = require('psi');
 var site = "http://meedan.com";
-var w3cjs = require('gulp-w3cjs');
 var notify = require("gulp-notify");
 
+// Send error to notification center with gulp-notify
 handleErrors = function () {
-
   var args = Array.prototype.slice.call(arguments);
-
-  // Send error to notification center with gulp-notify
   notify.onError({
     title: "Compile Error",
   }).apply(this, args);
-
-  // Keep gulp from hanging on this task
   this.emit('end');
 };
 
+// SVG files get special handling
 gulp.task('bundle-svg', function () {
-  // SVG files get special handling
   gulp.src('source/images/vector/*.svg')
     .pipe(svgmin())
     .pipe(svgstore())
     .pipe(gulp.dest('source/images/vector/'))
 });
 
+// Check our pagespeed score (in production)
 gulp.task('pagespeed', function () {
   return psi(site, {
     nokey: 'true',
@@ -35,11 +31,4 @@ gulp.task('pagespeed', function () {
     console.log(data.score);
     console.log(data.pageStats);
   });
-});
-
-// HTML Validator
-gulp.task('validate', function () {
-  gulp.src(['build/*.html', '!build/_*.html'])
-    .on('error', handleErrors)
-    .pipe(w3cjs());
 });
