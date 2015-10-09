4dc6f86a13f5a5cea90902673d1fbb50dda5a3f7
xrwang <xiaowei.wang@post.harvard.edu>
2015-10-09 15:12:00 -0400

    about pageeee

---
+++ source/javascripts/softscroll.js
@@ -0,0 +1,23 @@
+/*! softscroll */
+(function ($) {
+  $(document).ready(function(){
+
+  // hide .navbar first
+  $(".navbar").hide();
+
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+      if ($(this).scrollTop() > 300) {
+        $('.navbar').fadeIn();
+      } else {
+        $('.navbar').fadeOut();
+      }
+    });
+
+
+  });
+
+});
+  }(jQuery));
