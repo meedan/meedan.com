609a5ed6a4df87344c8e5379d2745e2f644cac06
Chris Blow <chris@meedan.com>
2015-10-10 16:55:36 -0700

    use local js for a minute (on an airplane)

--- config.rb
+++ config.rb
@@ -1,13 +1,13 @@
 helpers do
 
 	# Language toggle WIP
-  # 
+  #
 	# https://forum.middlemanapp.com/t/i18n-list-of-language-siblings-and-links-to-them/978/2
   def translated_url(locale)
     # Assuming /:locale/page.html
     page_name = @page_id.split("/", 2).last.sub(/\..*$/, '')
     untranslated_path = t(:paths).index(page_name)
-    
+
     begin
       translated = I18n.translate!("paths.#{untranslated_path}", locale: locale)
     rescue I18n::MissingTranslationData
@@ -41,5 +41,5 @@ end
 configure :build do
 	activate :minify_css
 	activate :minify_javascript
-	activate :asset_hash
+	# activate :asset_hash
 end
