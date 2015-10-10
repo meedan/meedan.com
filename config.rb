b9708138edc0a911074536605f1fc66316988449
Chris Blow <chris@meedan.com>
2015-10-10 15:21:07 -0700

    Improve HTML formatting, rerender build

--- config.rb
+++ config.rb
@@ -1,53 +1,11 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-configure :development do
-	activate :livereload
-end
-
 helpers do
 
 	# Language toggle WIP
+  # 
 	# https://forum.middlemanapp.com/t/i18n-list-of-language-siblings-and-links-to-them/978/2
   def translated_url(locale)
     # Assuming /:locale/page.html
     page_name = @page_id.split("/", 2).last.sub(/\..*$/, '')
-
     untranslated_path = t(:paths).index(page_name)
     
     begin
@@ -63,36 +21,25 @@ helpers do
   end
 end
 
+# Assets setup
 set :css_dir, 'css'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
-
-activate :directory_indexes
-
-activate :i18n, :mount_at_root => "en"
-
-redirect "index.html", :to => "en/index.html"
-
-
-# Asset pipeline
 activate :sprockets
 
-# Build-specific configuration
+# Routing
+activate :directory_indexes
+activate :i18n, :mount_at_root => "en"
+redirect "index.html", :to => "en/index.html"
+
+# Dev config only (while editing templates)
+configure :development do
+  activate :livereload
+end
+
+# Build config only (when generating the static files)
 configure :build do
-	# For example, change the Compass output style for deployment
-	# activate :minify_css
-
-	# Minify Javascript on build
-	# activate :minify_javascript
-
-	# Enable cache buster
-	# activate :asset_hash
-
-	# Use relative URLs
-	# activate :relative_assets
-
-	# Or use a different image path
-	# set :http_prefix, "/Content/images/"
+	activate :minify_css
+	activate :minify_javascript
+	activate :asset_hash
 end
