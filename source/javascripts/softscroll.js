305fd3fa0bfa5d255c1ea565de92ebd7e44af33e
Chris Blow <chris@meedan.com>
2015-10-11 17:11:08 -0400

    add localization helpers, app.js, and only do header reveal once

--- source/javascripts/softscroll.js
+++ source/javascripts/softscroll.js
@@ -1,17 +1,15 @@
 (function ($) {
   $(document).ready(function () {
 
-    // hide .navbar first
     $(".navbar").hide();
+
+    var scroll_position = $(this).scrollTop();
 
     // fade in .navbar
     $(function () {
       $(window).scroll(function () {
-        // set distance user needs to scroll before we fadeIn navbar
-        if ($(this).scrollTop() > 300) {
-          $('.navbar').fadeIn();
-        } else {
-          $('.navbar').fadeOut();
+        if (scroll_position > 300) {
+          $(".navbar").fadeIn();
         }
       });
     });
