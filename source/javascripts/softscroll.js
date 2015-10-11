c6a41b79f91c3dd7a8bae36176396a1f4181781b
Chris Blow <chris@meedan.com>
2015-10-10 17:08:50 -0700

    refactor language toggle as locale nav

--- source/javascripts/softscroll.js
+++ source/javascripts/softscroll.js
@@ -1,23 +1,19 @@
-/*! softscroll */
 (function ($) {
-  $(document).ready(function(){
+  $(document).ready(function () {
 
-  // hide .navbar first
-  $(".navbar").hide();
+    // hide .navbar first
+    $(".navbar").hide();
 
-  // fade in .navbar
-  $(function () {
-    $(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
-      if ($(this).scrollTop() > 300) {
-        $('.navbar').fadeIn();
-      } else {
-        $('.navbar').fadeOut();
-      }
+    // fade in .navbar
+    $(function () {
+      $(window).scroll(function () {
+        // set distance user needs to scroll before we fadeIn navbar
+        if ($(this).scrollTop() > 300) {
+          $('.navbar').fadeIn();
+        } else {
+          $('.navbar').fadeOut();
+        }
+      });
     });
-
-
   });
-
-});
-  }(jQuery));
+}(jQuery));
