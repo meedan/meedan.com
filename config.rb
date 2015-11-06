962d2fec733760f99b6f20ca117439e9c1dc3b0d
Chris Blow <chris@meedan.com>
2015-11-05 19:49:55 -0800

    remove old images, standardize image names, template refactor

--- config.rb
+++ config.rb
@@ -1,31 +1,39 @@
-require "lib/localization_helpers"
-helpers LocalizationHelpers
-
 # Assets setup
+#
 set :css_dir, 'css'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :partials_dir, 'partials'
 
 # Make sprockets aware of bower
+#
 # note that this does not seem to add them to the sass Load_path,
 # so for now we have to use "../../" syntax in our @imports — 2015 Oct 14 CGB
 bower_path = File.join root, 'bower_components'
 sprockets.append_path bower_path
 
 # Routing
+#
 activate :directory_indexes
-activate :i18n, :mount_at_root => "en"
 redirect "index.html", :to => "en/index.html"
-# redirect "/checkdesk/", :to => "en/checkdesk/index.html"
+activate :i18n, :mount_at_root => "en"
 
-# Dev config only (while editing templates)
+# I18n Fallbacks
+#
+# Image names are only kept in the en.yml
+I18n.fallbacks.map(:ar => :en)
+
+# Dev config only
+#
+# (while editing templates)
 configure :development do
   activate :livereload
 end
 
-# Build config only (when generating the static files)
+# Build config
+#
+# (when generating the static files)
 configure :build do
 	activate :minify_css
 	activate :minify_javascript
-	# activate :asset_hash
 end
