b67ff44c68f8ab84e2bf80e47680de20b54a7341
Chris Blow <chris@meedan.com>
2016-02-03 12:08:56 -0800

    Remove stray middleman-OGP source code

--- middleman-ogp-1.0.4/lib/middleman-ogp.rb
+++
@@ -1,7 +0,0 @@
-require "middleman-core"
-require "middleman-ogp/version"
-
-::Middleman::Extensions.register(:ogp) do
-  require "middleman-ogp/extension"
-  ::Middleman::OGP::OGPExtension
-end
--- middleman-ogp-1.0.4/lib/middleman-ogp/version.rb
+++
@@ -1,5 +0,0 @@
-module Middleman
-  module OGP
-    VERSION = "1.0.4"
-  end
-end
