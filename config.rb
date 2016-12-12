28c1108e9203b877ab781675d03de62b30ab2b51
Chris Blow <chris@meedan.com>
2016-12-12 14:44:06 -0800

    Improve middleman config

--- config.rb
+++ config.rb
@@ -5,17 +5,7 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :partials_dir, 'partials'
 
-# Make sprockets aware of bower
-#
-# note that this does not seem to add them to the sass Load_path,
-# so for now we have to use "../../" syntax in our @imports — 2015 Oct 14 CGB
-#
-bower_path = File.join root, 'bower_components'
-sprockets.append_path bower_path
-
 # Livereload
-#
-# FIXME: it doesn't work very well with middleman; sluggish for me. — 2015 Dec 18 CGB
 #
 activate :livereload
 
@@ -30,9 +20,6 @@ activate :i18n, :mount_at_root => "en"
 # See: https://github.com/ngs/middleman-ogp
 #
 activate :ogp do |ogp|
-  #
-  # register namespace with default options
-  #
   ogp.namespaces = {
     og: data.ogp.og
   }
@@ -41,16 +28,14 @@ end
 
 # I18n Fallbacks
 #
-# Image names are only kept in the en.yml
+# Fallback if a translation is not available.
 #
 I18n.fallbacks.map(:ar => :en)
 
 # Build config
 #
-# (when generating the static files)
-#
 configure :build do
 	activate :minify_css
 	activate :minify_javascript
-    activate :autoprefixer
+  activate :autoprefixer
 end
