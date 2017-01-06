c90984e3bfc600c932b1d57e8b2201454713ac01
Chris Blow <chris@meedan.com>
2017-01-06 15:06:20 -0800

    Improve redirection and related tests

--- test/pretest.rb
+++ test/pretest.rb
@@ -1,9 +1,11 @@
 require_relative './helpers'
 
-text_banner 'Building the site and starting test server...'
+text_banner 'Rebuilding the site and starting test server...'
+
+system `rm -rf build`
 
 # Rebuild the site to ensure we test the latest static output
-system 'npm build'
+system 'npm run build'
 
 # Start the server, and note the server PID so we can stop it post test
 system 'node server.js & echo $! > test/server.pid'
