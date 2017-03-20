71d06352735c574a0d01e089d06828cec1373130
Chris Blow <chris@meedan.com>
2017-03-20 13:59:54 -0700

    Fix donorbox link, add ar (placeholder) images, remove extra IDs in SVG

--- test/test.rb
+++ test/test.rb
@@ -4,21 +4,7 @@ require_relative './helpers'
 #
 system 'casperjs test --log-level=error test/casper.js --engine=slimerjs'
 
-# 2. Run wraith to check for CSS regressions
-#
-text_banner(" Starting Wraith CSS regression tests")
-
-# Check for screenshot history first, if it's missing, create it so this test will work next time
-# See http://bbc-news.github.io/wraith
-if Dir.exists?('./test/screenshot_history')
-	text_banner("Creating latest screenshots and comparing to history...")
-	system 'wraith latest test/wraith.yml'
-else
-	text_banner("There is no screenshot history yet. Creating it...")
-	system 'wraith history test/wraith.yml'
-end
-
-# 3. Run HTML Proofer to check for broken links
+# 2. Run HTML Proofer to check for broken links
 #
 text_banner("Starting HTML-Proofer tests")
 HTMLProofer.check_directory("./build", {
@@ -27,7 +13,7 @@ HTMLProofer.check_directory("./build", {
   :url_ignore => LEGACY_PATHS
   }).run
 
-# 4. Check localization of the site
+# 3. Check localization of the site
 #
 text_banner("Starting localization check")
 require_relative "./localization.rb"
--- test/wraith-helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (casperjs, ready) {
-  // Hide dynamic sections to avoid unnecssary failures (eg. the Medium.com embeds)
-  //
-  var styles = document.createElement("style");
-  var head = document.getElementsByTagName("head")[0];
-  styles.innerHTML = '.medium-channels { display: none !important; }';
-  head.appendChild(styles);
-}
