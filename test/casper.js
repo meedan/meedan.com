952b896dde4c863ad3ea08409c0aeb5c40c88ab2
Chris Blow <chris@meedan.com>
2017-04-21 18:08:11 -0700

    Update tests to reflect temporary localization plan

--- test/casper.js
+++ test/casper.js
@@ -67,19 +67,21 @@ casper.test.begin('/bridge redirects to a default language-specific page', 2, fu
 
 // Test RTL element
 //
-casper.test.begin('/check arabic has rtl', 1, function suite(test) {
-  casper.start(localhostURL + "/ar/check/", function () {
-    this.wait(1000, function () {
-      this.echo("waited for 1 seconds");
-      var dump = require('utils').dump;
-      test.assertMatch(this.getElementAttribute('body', 'dir'), /^rtl$/i);
-    });
-  });
+// Disabled temporarily until we localize this page again. See below test of expected functionality. 2017-4-21 CGB
+//
+// casper.test.begin('/check arabic has rtl', 1, function suite(test) {
+//   casper.start(localhostURL + "/ar/check/", function () {
+//     this.wait(1000, function () {
+//       this.echo("waited for 1 seconds");
+//       var dump = require('utils').dump;
+//       test.assertMatch(this.getElementAttribute('body', 'dir'), /^rtl$/i);
+//     });
+//   });
 
-  casper.run(function () {
-    test.done();
-  });
-});
+//   casper.run(function () {
+//     test.done();
+//   });
+// });
 
 // Test (temporary) redirection of /ar check page to /en check page.
 //
