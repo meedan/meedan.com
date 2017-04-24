2fa13ed24badd4659f762c342b5a1d1a813f1ee5
Chris Blow <chris@meedan.com>
2017-04-24 16:11:10 -0700

    Redirect stdout when running the test server

--- test/test.rb
+++ test/test.rb
@@ -26,10 +26,7 @@ system 'npm run build'
 text_banner 'Starting server...'
 # Start the server, and note the server PID so we can stop it post test
 # system 'node server.js && echo $! > tmp/server.pid'
-system 'node server.js'
-
-text_banner 'Pretest complete...'
-
+system 'node server.js > /dev/null 2>&1'
 
 text_banner 'Starting test...'
 
