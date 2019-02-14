4444f31b8bf936e4b1f7e98158d0e193d3d706a4
Abdul Rehman Khawar <designbysoil@gmail.com>
2019-02-13 16:51:39 -0800

    Adding user guide template.

--- js/main.js
+++ js/main.js
@@ -6,4 +6,35 @@ $(document).ready(function() {
 		dots: true,
 	});
 
+
+	// Image
+	$('a.media-link').magnificPopup({
+		type:'image',
+		closeOnContentClick: true,
+		image: {
+
+	  markup: '<div class="mfp-figure">'+
+	            '<div class="mfp-close"></div>'+
+	            '<div class="mfp-img"></div>'+
+	            '<div class="mfp-bottom-bar">'+
+	              '<div class="mfp-title"></div>'+
+	              '<div class="mfp-counter"></div>'+
+	            '</div>'+
+	          '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+	  cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+
+	  titleSrc: 'alt', // Attribute of the target element that contains caption for the slide.
+	  // Or the function that should return the title. For example:
+	  // titleSrc: function(item) {
+	  //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+	  // }
+
+	  verticalFit: true, // Fits image in area vertically
+
+	  tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+	}
+
+	});
+
 });
