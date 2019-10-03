dc2531c014c897683c08617c02c86d729255beae
infojunkie <karim.ratib@gmail.com>
2019-10-03 16:06:26 +0100

    Check Message TOS es

--- config.rb
+++ config.rb
@@ -15,7 +15,9 @@ activate :directory_indexes
 redirect "index.html", :to => "en/index.html"
 activate :i18n, :mount_at_root => "en"
 page "**/*_tos.html", :directory_index => false
+page "**/*_tos.*.html", :directory_index => false
 page "**/*_privacy.html", :directory_index => false
+page "**/*_privacy.*.html", :directory_index => false
 
 
 # Open Graph Tags
