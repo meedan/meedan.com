5561d39c091bb644ead09d4fc423b150fe89ab4e
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-08-11 20:54:39 -0700

    Scrollspy fix for chrome.

--- js/sticky.js
+++ js/sticky.js
@@ -18,22 +18,18 @@ function myFunction() {
 
 
 // ScrollSpy that highlights the navigation item based on which section is in view
-window.addEventListener('DOMContentLoaded', () => {
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const id = entry.target.getAttribute('id');
+    if (entry.intersectionRatio > 0) {
+      document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+    } else {
+      document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+    }
+  });
+});
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
-	});
-
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
-	});
-	
+// Track all sections that have an `id` applied
+document.querySelectorAll('section[id]').forEach((section) => {
+  observer.observe(section);
 });
