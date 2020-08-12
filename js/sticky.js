6cb29557329af9f3d61bc233fc02a2202dfd1233
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-08-11 21:24:07 -0700

    Scrollspy fix for chrome. Part 2.

--- js/sticky.js
+++ js/sticky.js
@@ -21,6 +21,7 @@ function myFunction() {
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     const id = entry.target.getAttribute('id');
+    
     if (entry.intersectionRatio > 0) {
       document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
     } else {
