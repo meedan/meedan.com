00dab30bdbc56a54b01f6823f610cf154ab6daa6
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-09-04 12:10:17 -0700

    Topic pages dropdown navigation.

---
+++ js/topic.js
@@ -0,0 +1,19 @@
+const select = document.querySelector(".select-topic");
+const options = document.querySelectorAll(".select-topic option");
+ 
+// 1
+select.addEventListener("change", function() {
+  const url = this.options[this.selectedIndex].dataset.url;
+  if(url) {
+    location.href = url;
+  }
+});
+ 
+// 2
+for(const option of options) {
+  const url = option.dataset.url;
+  if(location.href.includes(url)) {
+    option.setAttribute("selected", "");
+    break;
+  }
+}
